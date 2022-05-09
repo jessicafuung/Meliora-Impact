93c80ebac6d3d2b7ab564c0e7cb5c14c5a25b1c1
Fabian Solheim <fabian.j.solheim@gmail.com>
2022-05-09 11:20:38 +0200

    Backend ready

---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Meliora Backend")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
+
+export default app
