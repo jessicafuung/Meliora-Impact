d57a6419b14d944444f57640ba3bd41a609184c2
Fabian Solheim <fabian.j.solheim@gmail.com>
2022-05-09 11:11:44 +0200

    server package.json remaining

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Logo from "./components/assets/Logo"
 
 
 const App = () => {
     return(
-        <>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div style={{textAlign: "center"}}>
+        <Logo/>
         <h1>Hello Meliora</h1>
-        </>
+        </div>
+        </div>
     )
 }
 
