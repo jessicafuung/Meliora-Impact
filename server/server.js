6f5305fb9dda603c41d1f5202a86cf22b55195d9
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-10 11:31:21 +0200

    created /api/bookings

--- server/server.js
+++ server/server.js
@@ -2,15 +2,21 @@ import express from "express"
 import * as path from "path";
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb";
-import {FAQApi} from "./FAQApi.js";
+import {FAQApi} from "./api/FAQApi.js";
+import {BookingApi} from "./api/BookingApi.js";
 
 dotenv.config();
 const app = express()
 const mongoClient = new MongoClient(process.env.MONGODB_URL);
 
 mongoClient.connect().then(async () => {
-    console.log("Connected to mongodb :)");
-    app.use("/api/faq", FAQApi(mongoClient.db(process.env.MONGODB_DATABASE)));
+    console.log("Connected to mongodb (FAQ)");
+    app.use("/api/faqs", FAQApi(mongoClient.db(process.env.MONGODB_DATABASE)));
+});
+
+mongoClient.connect().then(async () => {
+    console.log("Connected to mongodb (Booking)");
+    app.use("/api/bookings", BookingApi(mongoClient.db(process.env.MONGODB_DATABASE)));
 });
 
 app.get("/", (req, res) => {
