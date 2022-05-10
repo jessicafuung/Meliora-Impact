ef6cc8154355e8f9d33ad68d1bfad67c5a895fe2
Kenneth Kristiansen <kennethk2014@gmail.com>
2022-05-10 15:57:53 +0200

    new component

---
+++ client/src/pages/TestPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CustomHeadline from '../components/CustomHeadline/CustomHeadlineComponent';
+
+const TestPage = () => {
+
+    return (
+        <div>
+            <h3>TestPage</h3>
+            <CustomHeadline>Hello</CustomHeadline>
+        </div>
+    )
+}
+
+export default TestPage;
