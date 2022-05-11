5e70f4f89bad3170a11744f53d218d23e559695d
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-11 14:33:10 +0200

    client post selected date to /api/bookings

--- client/src/pages/booking.jsx
+++ client/src/pages/booking.jsx
@@ -1,15 +1,32 @@
-import React, {useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import "date-fns"
+import moment from 'moment';
 import Grid from "@material-ui/core/Grid"
 import DateFnsUtils from "@date-io/date-fns"
-import moment from 'moment';
 import {KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
 
+import {ApiContext} from "../../lib/useContext";
+
 export function Booking() {
-    /* todays date */
-    const [selectedDate, setSelectedDate] = useState(
-        new Date()
-    )
+    const { postBooking } = useContext(ApiContext)
+
+    const [selectedDate, setSelectedDate] = useState( new Date() )
+    const [date, setDate] = useState("")
+
+    /* Triggered everytime a date is selected. Todays date by default. */
+    /* Convert to ISO format, then again formatting it again by using 'moment' */
+    useEffect(() => {
+        const iso = new Date(selectedDate.getTime() - selectedDate.getTimezoneOffset() * 60 * 1000);
+
+        const momentFormat = moment(iso).format("DD.MM.YYYY");
+        setDate(momentFormat)
+    }, [selectedDate])
+
+    /* Triggered by form onSubmit */
+    function handleSubmit(e) {
+        e.preventDefault()
+        postBooking({date})
+    }
 
     const handleDateChange = (date) => {
         setSelectedDate(date)
@@ -19,37 +36,39 @@ export function Booking() {
         return date.getDay() === 0 || date.getDay() === 6;
     }
 
-    function convertUTCDateToLocalDate(date) {
-        const iso = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
-        const outputDate = moment(iso).format("dddd, DD.MM.YYYY");
-        console.log("Moment: "+outputDate)
-        return iso
-    }
-
     return (
         <>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justifyContent='space-around'>
-                <KeyboardDatePicker
-                    variant='static'
-                    id='date-picker'
-                    label='Date Picker'
-                    format='dd/MM/yyyy'
-                    margin='normal'
-                    disablePast
-                    hintText="Weekends Disabled"
-                    shouldDisableDate={disableWeekends}
-                    disableToolbar
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{'aria-label':'change date'}}
-                />
-                <KeyboardTimePicker />
-            </Grid>
-        </MuiPickersUtilsProvider>
-        <h3>Your date (date picker): {selectedDate.getDate()} {selectedDate.getMonth()} {selectedDate.getFullYear()}</h3>
-            <h3>ISO format: {convertUTCDateToLocalDate(selectedDate).toISOString()}</h3>
-    </>
+            <form onSubmit={handleSubmit}>
+                <ul>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <Grid container justifyContent='space-around'>
+                            <KeyboardDatePicker
+                                variant='static'
+                                id='date-picker'
+                                label='Date Picker'
+                                format='dd/MM/yyyy'
+                                margin='normal'
+                                disablePast
+                                hintText="Weekends Disabled"
+                                shouldDisableDate={disableWeekends}
+                                disableToolbar
+                                value={selectedDate}
+                                onChange={handleDateChange}
+                                KeyboardButtonProps={{'aria-label':'change date'}}
+                            />
+
+                            <KeyboardTimePicker
+
+
+                            />
+                        </Grid>
+                    </MuiPickersUtilsProvider>
+                </ul>
+
+                <button>Book</button>
+            </form>
+        </>
     );
 
 }
+
