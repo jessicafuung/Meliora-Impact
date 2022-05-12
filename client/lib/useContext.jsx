beab100d8cb3058fc6095d01adb6f9f69cb83af7
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-12 14:56:15 +0200

    / GET from /api/bookings? with query

--- client/lib/useContext.jsx
+++ client/lib/useContext.jsx
@@ -4,6 +4,11 @@ import { postJSON } from "./postJSON";
 
 export const ApiContext = React.createContext({
     async postBooking(booking) {
+        /* / POST req to /api/bookings */
         return await postJSON("/api/bookings", booking)
+    },
+    async listTimes(query) {
+        /* / GET req to /api/bookings (with filter) */
+        return await fetchJSON("/api/bookings?" + new URLSearchParams(query));
     }
 });
