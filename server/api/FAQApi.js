c2be6ae70f6f69d0fabc06fdbe6040cd98c6b254
neutix <martin.wedvik@gmail.com>
2022-05-18 18:47:48 +0200

    search

--- server/api/FAQApi.js
+++ server/api/FAQApi.js
@@ -3,34 +3,32 @@ import {Router} from "express";
 export function FAQApi(mongoDatabase) {
     const router = new Router();
 
+
     router.get("/", async (req, res) => {
 
+        function onlyUnique(value, index, self){
+            return self.indexOf(value) === index;
+        }
+
         const { search } = req.query
-        console.log(search)
+        const searchArray = []
+        let replaceSpace = search.split(' ')
 
-        let testSearch = [
-            "can", "who", "join"
-        ]
-
-        if( search ) {
+        if(search) {
             const faqs = await mongoDatabase
                 .collection("test")
                 .find()
-                .toArray();
-            let searchArray = faqs.filter((q) => {
-
-                return q.question.toLowerCase().includes(
-                    search )
+                .toArray()
+            faqs.filter((q) => {
+                for (let item in replaceSpace){
+                    if(q.question.toLowerCase().includes(replaceSpace[item]) === true) {
+                        searchArray.push(q)
+                    }
+                }
             })
-            /*
-            const loopTest = testSearch.forEach(item => {
-                "{item}"
-            })
-
-             */
-
-            console.log(searchArray)
-            res.json(searchArray)
+            let filteredArray = searchArray.filter(onlyUnique)
+            res.json(filteredArray)
+            searchArray.length = 0
         }else{
             const faqs = await mongoDatabase
                 .collection("test")
