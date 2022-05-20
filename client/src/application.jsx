feda5100e88097e412fafc2aa51a0dbceb33fa67
attichouse <77102070+attichouse@users.noreply.github.com>
2022-05-20 12:46:30 +0200

    added navbar and footer to application.jsx so they show up on every page

--- client/src/application.jsx
+++ client/src/application.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import { Home } from "./pages/Home/Home";
-import { Contact } from "./contact";
+import { ChakraProvider } from "@chakra-ui/react";
 import TestPage from "./pages/TestPage";
 import { FaqPage } from "./pages/FaqPage/FaqPage";
 import Navbar from "./components/Navbar/Navbar";
@@ -10,18 +10,22 @@ import theme from "./styling/theme";
 import { ListCases } from "./listCases";
 import AnimationContainer from "./components/AnimationContainer/AnimationContainer";
 import Header from "./components/navbar";
-import {Contact} from './contact';
-import {KnowledgeCases} from "./knowledge";
+import { Contact } from "./contact";
+import { KnowledgeCases } from "./knowledge";
 import PrivacyPage from "./pages/PrivacyPage/PrivacyPage";
 import { AboutUsPage } from "./pages/AboutUsPage";
 import { GetFaq } from "./pages/Faq/Faq";
 import { CorporateMember } from "./pages/CorporateMember/corporateMember";
+import { Footer } from "./components/Footer/footer";
 
 export function Application() {
   return (
     <ThemeProvider theme={theme}>
       <AnimationContainer>
         <BrowserRouter>
+          <header>
+            <Navbar />
+          </header>
           <Routes>
             <Route path={"/contact"} element={<Contact />} />
             <Route path={"/"} element={<CorporateMember />} />
@@ -30,7 +34,7 @@ export function Application() {
             <Route path={"/privacy"} element={<PrivacyPage />} />
             <Route path={"/about"} element={<AboutUsPage />} />
           </Routes>
-
+          <Footer />
         </BrowserRouter>
       </AnimationContainer>
     </ThemeProvider>
