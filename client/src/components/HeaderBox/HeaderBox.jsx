358db52d3376a1e8b1c06dcbbacdee87b74228bd
neutix <martin.wedvik@gmail.com>
2022-05-21 14:41:42 +0200

    HeaderBox

---
+++ client/src/components/HeaderBox/HeaderBox.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Headerbox.css"
+
+export function HeaderBox(headline, text){
+    return (
+        <div className="HeaderBox">
+            <div className="Headline">{headline}</div>
+            <div style={{
+                marginTop: 50,
+                fontSize: 24
+            }}>{text}</div>
+        </div>
+    )
+}
--- client/src/pages/Non-profitMember/nonProfitMember.jsx
+++ client/src/pages/Non-profitMember/nonProfitMember.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import "./nonProfitMember.css";
 import { Benefits } from "./benefits";
 import { Services } from "./services";
+import {HeaderBox} from "../../components/HeaderBox/HeaderBox";
 export function NonProfit() {
   return (
     <>
-      <div>
-        <div className="header">
-          <div>
-            <p className="top">t</p>
-            <p className="headline">
-              <strong>
-                Non-profit <em>members</em>
-              </strong>
-            </p>
-            <p className="description">
-              There's often no solid <em>philanthropic</em> strategy and
-              <br /> nonprofits are picked on an <em>ad-hoc basis.</em>
-            </p>
-          </div>
-        </div>
+      <div className="header">
+          {HeaderBox(
+              <h1 style={{
+                  color: "white",
+                  fontFamily: "Montserrat",
+              }}>Non-profit members</h1>,
+              <p style={{
+                  color: "white",
+                  fontFamily: "Montserrat",
+              }}>hello text</p>
+          )}
       </div>
       <div style={{ height: 100 }} />
       <Benefits />
