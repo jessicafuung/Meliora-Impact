32629c7b1cdf05562e8a85cc6a055c0b6fe3a19e
Neutix <72227602+Neutix@users.noreply.github.com>
2022-05-24 13:20:24 +0200

    Added box to knowledge

--- client/src/pages/CaseStudyKnowledge/CaseStudyKnowledgeHeader.jsx
+++ client/src/pages/CaseStudyKnowledge/CaseStudyKnowledgeHeader.jsx
@@ -1,27 +1,19 @@
 import React from "react";
 import "./CaseStudyKnowledge.css";
+import {HeaderBox} from "../../components/HeaderBox/HeaderBox";
 
 export function CaseStudyKnowledgeHeader() {
   return (
     <>
       <div className="headers">
-        <div>
-          <p className="headlines">
-            <strong>Knowledge</strong>
-          </p>
-          <p className="descriptions">
-            More than
-            <strong>
-              {" "}
-              <em>72 million</em>
-            </strong>{" "}
-            children of <br /> primary education age are{" "}
-            <strong>
-              <em>not</em>
-            </strong>{" "}
-            in school.
-          </p>
-        </div>
+        {HeaderBox(
+            "../../images/knowledgeBackground.png",
+            "Knowledge",
+            "white",
+            <p>More than <strong><em>72 million</em></strong> children of<br/>
+                primary education age are <strong><em>not</em></strong> in school.</p>,
+            "white"
+        )}
         <button className="consultationButton">
           <strong>Book a Consultation</strong>
         </button>
