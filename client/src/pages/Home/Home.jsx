044bf889ecb3d6045146a49ef9892d7912e60fb5
Kenneth Kristiansen <kennethk2014@gmail.com>
2022-05-30 14:02:48 +0200

    navbar and responsive front page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Home.css";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import {Box, Grid, Typography} from "@mui/material";
 import { members } from "../../components/MemberServiceCard/Members";
 import { MemberServiceCard } from "../../components/MemberServiceCard/memberServiceCard";
 import { sectors } from "../../components/SectorCard/sectors";
@@ -13,42 +12,27 @@ import { HeadlineWithUnderline } from "../../components/HeadlineWithUnderline/he
 export function Home() {
   return (
     <>
-      <Box className="home-header" sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <div />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <div>
-              <img
-                className="titleImage"
-                src="https://i.postimg.cc/Kjwnys4K/Meliora-title.png"
-              />
-              <p className="description" style={{ fontSize: 26 }}>
-                A digital platform where{" "}
-                <strong>
-                  <em>companies</em>
-                </strong>
-                <br />
-                can give to nonprofits in a more
-                <br />{" "}
-                <strong>
-                  <em>impactful</em>
-                </strong>
-                and{" "}
-                <strong>
-                  <em>sustainable</em>
-                </strong>{" "}
-                way
-              </p>
+    {/*HEADER*/}
+      <Box className="home-header" paddingBottom={{xs: 5, md: 10,}} paddingTop={{xs: 5, md: 15,}}>
+        <img className="header-image" src="https://images.unsplash.com/photo-1636320804382-912276801e97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" />
+        <Grid container direction="row-reverse" alignItems="end" >
+          <Grid item container justifyContent="end" xs={12} sm={6}>
+            <Box sx={{textAlign: {xs: 'center', sm: 'left'}}} className="text-container-header" paddingRight={{xs: 0, sm: 2}} >
+              <Typography gutterBottom variant="h2" color="white">Make an <span className="text-highlight">impact</span> with <span className="text-highlight">Meliora</span></Typography>
+              <Typography color="textPrimary" paragraph fontWeight={500} fontSize={16} >
+                A digital platform where companies can give to nonprofits in a more <span className="text-highlight">impactful and sustainable way</span>
+              </Typography>
               <button className="consultationBtn">
                 <strong>Book a Consultation</strong>
               </button>
-            </div>
+            </Box>
           </Grid>
         </Grid>
       </Box>
-      <Box>
+
+
+
+       <Box py={{xs: 5, md: 10,}}>
         <Grid container justifyContent="center">
           <Grid item>
             <div className="text-box">
@@ -62,21 +46,30 @@ export function Home() {
             </div>
           </Grid>
         </Grid>
-        <Grid container justifyContent="center">
-          {members.map((member) => (
-            <>
-              <MemberServiceCard
-                key={member.title}
-                title={member.title}
-                content={member.content}
-                icon={member.icon}
-                linkTo={member.linkTo}
-              />
-            </>
-          ))}
-        </Grid>
+        <Box py={5} px={{xs: 2}}>
+           <Grid container justifyContent="center" spacing={5}>
+            {members.map((member) => (
+                <Grid item xs={12} sm={6} md={5}>
+                    <>
+                      <MemberServiceCard
+                        key={member.title}
+                        title={member.title}
+                        content={member.content}
+                        icon={member.icon}
+                        linkTo={member.linkTo}
+                      />
+                    </>
+                </Grid>
+            ))}
+            </Grid>
+
+               </Box>
+
       </Box>
-      <Box>
+
+
+      {/*SECTOR SECTION*/}
+      <Box paddingTop={{xs: 5, md: 10,}} >
         <Grid
           container
           flexDirection="column"
@@ -101,7 +94,24 @@ export function Home() {
           </Grid>
         </Grid>
       </Box>
-      <Grid container justifyContent="center">
+
+
+      <Box paddingBottom={{xs: 5, md: 15}} px={{xs: 2}} paddingTop={5}>
+      <Grid container justifyContent="center" spacing={5}>
+      {sectors.map((sector) => (
+                <Grid item xs={12} sm={6} md={5}>
+                  <SectorCard
+                    key={sector.title}
+                    title={sector.title}
+                    content={sector.content}
+                    image={sector.image}
+                    linkTo={sector.linkTo}
+                  />
+                </Grid>
+              ))}
+      </Grid>
+      </Box>
+      {/* <Grid container justifyContent="center">
         {sectors.map((sector) => (
           <>
             <SectorCard
@@ -113,14 +123,16 @@ export function Home() {
             />
           </>
         ))}
-      </Grid>
-      <Box>
+      </Grid> */}
+
+      {/*BE THE CHANGE SECTION*/}
+      <Box py={{xs: 5, md: 10,}}>
         <BeTheChange />
         <div style={{ marginTop: 40 }} />
       </Box>
       <Box>
         {HeadlineWithUnderline("Partners", 80, "#034F7A", 40, "#034F7A")}
-        <PartnerCard />
+{/*         <PartnerCard /> */}
         <div style={{ marginBottom: 40 }} />
       </Box>
     </>
--- client/src/styling/theme.js
+++ client/src/styling/theme.js
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material";
 import { green, purple } from "@mui/material/colors";
 
 const primary = "#034F7A";
@@ -16,9 +16,57 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Montserrat", sans-serif',
-    fontSize: 16,
-  },
+          fontFamily: "Montserrat",
+          fontSize: "16px",
+               },
 });
+theme.typography.h1 = {
+    fontSize: '4rem',
+    fontFamily: 'Montserrat',
+    '@media (min-width:375px)': {
+      fontSize: '1.8rem',
+    },
+    '@media (max-width:375px)': {
+      fontSize: '1.8rem',
+    },
+    '@media (min-width:768px)': {
+      fontSize: '2.7rem',
+    },
+    '@media (min-width: 900px)':{
+      fontSize: "3.7rem",
+    },
+    '@media (min-width: 1440px)':{
+      fontSize: "4.5rem",
+    }
+}
+theme.typography.h2 = {
+    fontSize: '28px',
+    fontFamily: 'Montserrat',
+    '@media (min-width:600px)': {
+      fontSize: '2.5rem',
+
+    },
+    '@media (min-width: 900px)':{
+      fontSize: "3.5rem",
+    }
+}
+theme.typography.h3 = {
+    fontSize: '21px',
+    fontFamily: 'Montserrat',
+    '@media (min-width:600px)': {
+      fontSize: '1.5rem',
+    },
+    '@media (min-width:768px)': {
+      fontSize: '2rem',
+    },
+    '@media (min-width:1024px)': {
+      fontSize: '2.2rem',
+    },
+    '@media (min-width:1440px)': {
+      fontSize: '2.4rem',
+    },
+}
+theme.paragraph
+
 
 export default theme;
