570029f6dc8f593c4926f11364d1c7f245fdb4a1
Jessica Fung <jessica-fcc@hotmail.com>
2022-05-30 14:31:16 +0200

    progress bar

---
+++ client/src/components/progressbar.jsx
@@ -0,0 +1,24 @@
+import React, {useContext, useState} from "react";
+import { Stepper, Step, StepLabel } from "@material-ui/core";
+
+export function Progressbar({activeStep}) {
+
+    return (
+        <div style={{ margin: 20 }}>
+            <Stepper activeStep={activeStep}>
+                <Step>
+                    <StepLabel>Step 1: Select a date</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Step 2: Choose time</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>Step 3: Fill the form</StepLabel>
+                </Step>
+                <Step>
+                    <StepLabel>All steps completed!</StepLabel>
+                </Step>
+            </Stepper>
+        </div>
+    );
+}
