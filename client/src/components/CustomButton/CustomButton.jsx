d43b4f07f84ea80c6068af1f8648c2e1eb8c28bd
Fabian Solheim <fabian.j.solheim@gmail.com>
2022-05-31 13:50:27 +0200

    Small fixes

--- client/src/components/CustomButton/CustomButton.jsx
+++ client/src/components/CustomButton/CustomButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
-const CustomButton = ({ children, variant, w, h, to, p }) => {
+const CustomButton = ({ children, variant, w, h, to, p, onClick, href}) => {
   if (to === undefined) {
     console.log(
       `%cCorrect usage: <CustomButton variant=\"contained | outlined | text\" to=\"/\">${children}</CustomButton>`,
@@ -11,10 +11,12 @@ const CustomButton = ({ children, variant, w, h, to, p }) => {
   }
 
   return (
+    //anchor tag for href
     <Button
+      onClick={onClick}
       variant={variant}
+      to={to}
       as={Link}
-      to={`${to}`}
       color="primary"
       style={{
         padding: p,
--- client/src/components/CustomEmployeeCard/CustomEmployeeCard.jsx
+++ client/src/components/CustomEmployeeCard/CustomEmployeeCard.jsx
@@ -1,10 +1,8 @@
-import { Avatar, Grid, Typography, Link as MuiLink } from "@mui/material";
+import { Avatar, Grid, Typography, Link, Button } from "@mui/material";
 import React from "react";
 import "./CustomEmployeeStyle.css";
-import { Box } from "@mui/system";
+import Box from "@mui/material/Box";
 import CustomButton from "../CustomButton/CustomButton";
-
-
 
 export function CustomEmployeeCard({
   employee: { name, image, position, about, linkedin },
@@ -27,12 +25,14 @@ export function CustomEmployeeCard({
             <Avatar sx={{ width: 56, height: 56 }} src={`${image}`} />
           </Grid>
           <Grid item container justifyContent="start" xs={8}>
-            <Typography mb={0} variant="h6" color="primary">
-              {name}
-            </Typography>
-            <Typography mb={0} paragraph color="textDisabled">
-              {position}
-            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography mb={0} variant="h6" color="primary">
+                {name}
+              </Typography>
+              <Typography mb={0} paragraph color="textDisabled">
+                {position}
+              </Typography>
+            </Box>
           </Grid>
         </Grid>
         <Box mt={3} pr={2} pl={2}>
@@ -40,12 +40,30 @@ export function CustomEmployeeCard({
             {about}
           </Typography>
         </Box>
-      
-         <div className={"buttonDiv"}>
-         <MuiLink style={{textDecoration: "none"}} href={linkedin}>
-          <CustomButton variant="outlined">Linked In</CustomButton>
-        </MuiLink>
-      </div> 
+
+        <div className={"buttonDiv"}>
+          <Button
+            variant="outlined"
+            target="_blank"
+            href={linkedin}
+            color="primary"
+            style={{
+              padding: "0 22px",
+              textDecoration: "none",
+              fontWeight: 500,
+              borderRadius: 0,
+              textTransform: "none",
+            }}
+            sx={{
+              ":hover": {
+                backgroundColor: "#034F7A",
+                color: "white",
+              },
+            }}
+          >
+            Linked In
+          </Button>
+        </div>
       </Box>
     </Box>
   );
