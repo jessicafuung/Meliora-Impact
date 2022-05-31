97fe4eec01a83fc8b1f5051596212a680fad8895
attichouse <77102070+attichouse@users.noreply.github.com>
2022-05-31 15:27:41 +0200

    contact button faq

--- client/src/pages/Faq/Faq.jsx
+++ client/src/pages/Faq/Faq.jsx
@@ -98,7 +98,7 @@ export function FaqPage() {
         <Grid item xs={12}>
           <article>
             <p>If you can't find your question, please complete the form</p>
-            <CustomButton variant="text">Contact</CustomButton>
+            <CustomButton to={"/contact"} variant="text">Contact</CustomButton>
           </article>
         </Grid>
       </Grid>
