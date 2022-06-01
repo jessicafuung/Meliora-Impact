927cc9cd2d7e95479e7257f19d3531ce8d26168b
neutix <martin.wedvik@gmail.com>
2022-06-01 16:57:24 +0200

    cases page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import Logo from "../assets/Logo";
 
-const pages = ["About", "Contact", "Booking"];
+const pages = ["About", "Contact", "Booking", "Cases"];
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
---
+++ client/src/pages/Cases/Cases.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { sectors } from "../../components/SectorCard/sectors";
+import { SectorCard } from "../../components/SectorCard/sectorCard";
+import { HeadlineWithUnderline } from "../../components/HeadlineWithUnderline/headlineWithUnderline";
+
+export function Cases() {
+  return (
+    <>
+      {HeadlineWithUnderline("Cases", 80, "#034F7A", 0, "#212121", "36px")}
+      <Grid container justifyContent="center" paddingTop={5}>
+        <Grid item xs={12} sm={4}>
+          <Container>
+            A lot of giving platforms leave us with the paradox of choice. Many
+            corporate giving systems don't thoroughly vet the projects and
+            nonprofits that are featured.
+            <br />
+            <br />
+            Our platform has two categories: knowledge and water. We know that
+            these two areas are vital to thriving societies—this is why our
+            portfolio focuses on them.
+          </Container>
+        </Grid>
+      </Grid>
+      <Box paddingBottom={{ xs: 5, md: 15 }} px={{ xs: 2 }} paddingTop={5}>
+        <Grid container justifyContent="center" spacing={6}>
+          {sectors.map((sector) => (
+            <Grid item lg={4.1} xs={12} sm={6} md={5}>
+              <SectorCard
+                key={sector.title}
+                title={sector.title}
+                content={sector.content}
+                image={sector.image}
+                linkTo={sector.linkTo}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
+}
