02890882868de8114abb0076f5302bc5f1bf9fe0
neutix <martin.wedvik@gmail.com>
2022-06-01 18:02:07 +0200

    Headerbox fix, Faq fontSize fix

--- client/src/pages/CaseStudyWater/CaseStudyWaterHeader.jsx
+++ client/src/pages/CaseStudyWater/CaseStudyWaterHeader.jsx
@@ -7,46 +7,32 @@ import CustomButton from "../../components/CustomButton/CustomButton";
 export function CaseStudyWaterHeader() {
   return (
     <>
-      <Grid container>
-        <Grid item className="headers">
-          <Box>
-            {HeaderBox(
-              "https://images.unsplash.com/photo-1596021876043-3ec2658c4477?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2874&q=80",
-              "Water",
-              "black",
-              <p>
-                <strong>
-                  <em>785 million</em>
-                </strong>{" "}
-                people in the world live{" "}
-                <strong>
-                  <em>without</em>
-                </strong>
-                <br /> clean water. <br />
-              </p>,
-              "black"
-            )}
-          </Box>
-        </Grid>
-        <Box
-          sx={{
-            position: "relative",
-            bottom: "2rem",
-            left: "50%",
-            transform: "translateX(-50%)",
-            marginTop: "12px",
-          }}
+      {HeaderBox(
+        "https://images.unsplash.com/photo-1596021876043-3ec2658c4477?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2874&q=80",
+        "Water",
+        "black",
+        <p>
+          <strong>
+            <em>785 million</em>
+          </strong>{" "}
+          people in the world live{" "}
+          <strong>
+            <em>without</em>
+          </strong>
+          <br /> clean water. <br />
+        </p>,
+        "black"
+      )}
+      <div style={{ textAlign: "center", marginTop: "-15px" }}>
+        <CustomButton
+          p="12px 22px"
+          to="/booking"
+          variant="contained"
+          className="consultationButton"
         >
-          <CustomButton
-            p="12px 22px"
-            to="/booking"
-            variant="contained"
-            className="consultationButton"
-          >
-            Book a Consultation
-          </CustomButton>
-        </Box>
-      </Grid>
+          Book a Consultation
+        </CustomButton>
+      </div>
     </>
   );
 }
