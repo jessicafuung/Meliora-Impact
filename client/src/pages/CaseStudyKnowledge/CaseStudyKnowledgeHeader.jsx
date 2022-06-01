ae503b9b82d0091de68710852e86f4efe8e0877a
Mikrokaos <74602893+Mikrokaos@users.noreply.github.com>
2022-06-01 12:10:58 +0200

    knowledge hero fixed

--- client/src/pages/CaseStudyKnowledge/CaseStudyKnowledgeHeader.jsx
+++ client/src/pages/CaseStudyKnowledge/CaseStudyKnowledgeHeader.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./CaseStudyKnowledge.css";
 import { HeaderBox } from "../../components/HeaderBox/HeaderBox";
+import {Box, Grid} from "@material-ui/core";
+import CustomButton from "../../components/CustomButton/CustomButton";
 
 export function CaseStudyKnowledgeHeader() {
   return (
     <>
-      <div className="headers">
+        <Grid container>
+      <Grid item xs={12} className="headers">
+          <Box>
         {HeaderBox(
-          "../../images/knowledgeBackground.png",
+          "https://images.unsplash.com/photo-1522661067900-ab829854a57f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2970&q=80",
           "Knowledge",
           "white",
           <p>
@@ -23,13 +27,23 @@ export function CaseStudyKnowledgeHeader() {
             </strong>{" "}
             in school.
             <br />
-            <button style={{ margin: 100 }} className="consultationButton">
-              <strong>Book a Consultation</strong>
-            </button>
           </p>,
           "white"
         )}
-      </div>
+          </Box>
+      </Grid>
+            <Box sx={{
+                position: "relative",
+                bottom: "2rem",
+                left: "50%",
+                transform: "translateX(-50%)",
+                marginTop: "12px"
+            }}>
+                <CustomButton p="0 22" to="/booking" variant="contained" className="consultationButton">
+                    Book a Consultation
+                </CustomButton>
+            </Box>
+        </Grid>
     </>
   );
 }
