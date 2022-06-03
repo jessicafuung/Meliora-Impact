d35afb7bc6ee8624e271d66a7bd59177f66ed646
Neutix <72227602+Neutix@users.noreply.github.com>
2022-06-03 13:53:19 +0200

    server test update

--- server/__tests__/bookingApi.test.js
+++ server/__tests__/bookingApi.test.js
@@ -26,4 +26,25 @@ describe("Booking tests", () => {
     request(app);
     expect(await request(app).get("/api/booking").expect(200));
   });
+  it("Adds new booking to database", async () => {
+    const companyName = "jestTest";
+    const contactPerson = "jestTest";
+    const date = "09.02.2022";
+    const email = "jestTest@test.com";
+    const message = "jestTest";
+    const telephone = "92929292";
+    const time = "08:30";
+    await request(app)
+      .post("/api/booking")
+      .send({
+        companyName,
+        contactPerson,
+        date,
+        email,
+        message,
+        telephone,
+        time,
+      })
+      .expect(200);
+  });
 });
