df4e947f6927fdcb65c590c9f683c36b3840597e
Mikrokaos <74602893+Mikrokaos@users.noreply.github.com>
2022-06-03 13:04:23 +0200

    removed duplicate breadcrumb tag in breadcrumbs.jsx

--- client/src/components/Breadcrumbs/breadcrumbs.jsx
+++ client/src/components/Breadcrumbs/breadcrumbs.jsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import Link from "@mui/material/Link";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import {Breadcrumbs, Container, Typography} from "@mui/material";
+import { Breadcrumbs, Container, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import "./breadcrumbs.css";
-
 
 const BreadcrumbComponent = () => {
   const { pathname } = useLocation();
@@ -19,18 +18,20 @@ const BreadcrumbComponent = () => {
 
   return (
     <>
-
       <Container>
-      <Breadcrumbs>
-      <Breadcrumbs className="breadcrumb">
-
-        <Link underline={"hover"} fontSize={12} alignContent="center" href="/">
-          <HomeIcon className="homeIcon" />
-        </Link>
-        <Typography fontSize={12} fontWeight="bold" paddingTop="3px">
-          {pathname.length > 1 ? path : "Home"}
-        </Typography>
-      </Breadcrumbs>
+        <Breadcrumbs className="breadcrumb">
+          <Link
+            underline={"hover"}
+            fontSize={12}
+            alignContent="center"
+            href="/"
+          >
+            <HomeIcon className="homeIcon" />
+          </Link>
+          <Typography fontSize={12} fontWeight="bold" paddingTop="3px">
+            {pathname.length > 1 ? path : "Home"}
+          </Typography>
+        </Breadcrumbs>
       </Container>
     </>
   );
