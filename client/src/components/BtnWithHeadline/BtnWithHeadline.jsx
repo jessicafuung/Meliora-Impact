d45ce985512adadfde8a4c13c364e5e70e124dfc
Mikrokaos <74602893+Mikrokaos@users.noreply.github.com>
2022-06-05 10:53:41 +0200

    fixed join us button in business and nonprofit page to link to booking

--- client/src/components/BtnWithHeadline/BtnWithHeadline.jsx
+++ client/src/components/BtnWithHeadline/BtnWithHeadline.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./BtnWithHeadline.css";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 export function BtnWithHeadline(headlineText, btnText) {
   return (
     <div>
       <h1 className="headlineText">{headlineText}</h1>
-      <button className="buttonUnderHeadline">{btnText}</button>
+      <Button
+        as={Link}
+        to="/booking"
+        style={{ textAlign: "center" }}
+        variant="contained"
+        className="buttonUnderHeadline"
+      >
+        {btnText}
+      </Button>
     </div>
   );
+
+  // <div>
+  //     <h1 className="headlineText">{headlineText}</h1>
+  //     <Button as={Link} to="/booking" className="buttonUnderHeadline">
+  //         {btnText}
+  //     </Button>
+  // </div>
 }
