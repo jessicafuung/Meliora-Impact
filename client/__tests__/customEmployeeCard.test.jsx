0764e73de8c25ee5b8048d56bcd0a929f4f9e2b7
Neutix <72227602+Neutix@users.noreply.github.com>
2022-06-07 13:59:43 +0200

    jest snapshot fix

--- client/__tests__/customEmployeeCard.test.jsx
+++ client/__tests__/customEmployeeCard.test.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { CustomEmployeeCard } from "../src/components/CustomEmployeeCard/CustomEmployeeCard";
+import { MemoryRouter } from "react-router-dom";
+import { useLoading } from "../lib/useLoader";
+
+const testData = [
+  {
+    name: "test",
+    image: "imageUrl",
+    position: "test",
+    about: "test",
+    linkedin: "test",
+  },
+];
 
 describe("customEmployeeCard test", () => {
   it("shows CustomEmployeeCard", () => {
     const domElement = document.createElement("Box");
-    ReactDOM.render(<div>{CustomEmployeeCard}</div>, domElement);
+    ReactDOM.render(
+      <MemoryRouter>
+        {testData.map((m) => (
+          <CustomEmployeeCard key={m.name} employee={testData} />
+        ))}
+      </MemoryRouter>,
+      domElement
+    );
     expect(domElement.innerHTML).toMatchSnapshot();
   });
 });
