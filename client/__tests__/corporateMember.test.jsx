8c6b184bdf1a9bb27f77faae9c6645cd7c58b704
Neutix <72227602+Neutix@users.noreply.github.com>
2022-06-07 11:40:02 +0200

    jest snapshot fix

--- client/__tests__/corporateMember.test.jsx
+++ client/__tests__/corporateMember.test.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { CorporateMember } from "../src/pages/CorporateMember/corporateMember";
+import { MemoryRouter } from "react-router-dom";
 
 describe("CorporateMember page", () => {
   it("snapshot corporateMember page", () => {
     const domElement = document.createElement("div");
-    ReactDOM.render(<CorporateMember />, domElement);
+    ReactDOM.render(
+      <MemoryRouter>
+        <CorporateMember />
+      </MemoryRouter>,
+      domElement
+    );
     expect(domElement.innerHTML).toMatchSnapshot();
   });
 });
