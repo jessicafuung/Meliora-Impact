2858199858c06b8b5d0b50549b749845d077c7bd
Mikrokaos <74602893+Mikrokaos@users.noreply.github.com>
2022-06-07 14:16:33 +0200

    faqQuestionCard.test.jsx passes with flying colors

--- client/__tests__/faqQuestionCard.test.jsx
+++ client/__tests__/faqQuestionCard.test.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { FaqQuestionCard } from "../src/components/FaqQuestionCard/FaqQuestionCard";
+import { MemoryRouter } from "react-router-dom";
+
+const testData = [
+  {
+    _id: "testData",
+    question: "testQuestion",
+    answer: "testAnswer",
+  },
+];
 
 describe("FaqQuestionCard test", () => {
   it("shows FaqQuestionCard", () => {
     const domElement = document.createElement("div");
-    ReactDOM.render(<div>{FaqQuestionCard}</div>, domElement);
+    ReactDOM.render(
+      <MemoryRouter>
+        {testData.map((test, index) => (
+          <FaqQuestionCard key={test._id} faqs={test} index={index} />
+        ))}
+      </MemoryRouter>,
+      domElement
+    );
     expect(domElement.innerHTML).toMatchSnapshot();
   });
 });
